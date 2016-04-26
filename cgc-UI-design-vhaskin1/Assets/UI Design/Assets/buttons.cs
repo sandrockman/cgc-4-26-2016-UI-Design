927214b52259160fefb933227411c9608d83ccde
svc-student <svc-student@student.directory.jccc>
2016-04-26 15:51:39 -0500

    finished, but scroll rect button has issues

---
+++ cgc-UI-design-vhaskin1/Assets/UI Design/Assets/buttons.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class buttons : MonoBehaviour {
+
+	public Text chatText;
+
+
+	void Awake ()
+
+	{
+        if(chatText != null)
+            chatText = chatText.GetComponent<Text> ();
+	
+	}
+
+public void PlayButton (int scene)
+	{
+
+		SceneManager.LoadScene (scene);
+
+	}
+
+public void ExitButton ()
+
+	{
+
+		Application.Quit ();
+	}
+
+    public void ToggleButton(bool mainMenu)
+    {
+
+        if (mainMenu)
+        {
+            int move = SceneManager.GetSceneByName("Video5-Toggles").buildIndex;
+            SceneManager.LoadScene(move);
+
+        }
+    }
+    public void _ToggleButtonVideo5(bool mainMenu)
+    {
+
+        if (mainMenu)
+        {
+            SceneManager.LoadScene(2);
+
+        }
+    }
+
+
+    public void ChatInput (InputField chat)
+	{
+
+		chatText.text = chat.text;
+	}
+
+
+}
